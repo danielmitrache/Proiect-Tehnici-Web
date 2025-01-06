a1539cf2c06b460aa71c195e951efaba049b244f
daniel21 <mitracheadaniel@gmail.com>
2025-01-06 21:30:13 +0000

    fixed some mobile css issues

--- website-header.js
+++ website-header.js
@@ -14,10 +14,28 @@ headerElement.innerHTML = `
         <a href="../Retete/retete.html">Rețete</a>
       </div>
       <div class="headerlinks">
-        <a>Mai Multe</a>
+        <a id="maimultelinks">Mai Multe</a>
         <div class="dropdown-content">
           <a href="../Contact%20Page/contact.html">Contact</a>
           <a href="../Own%20Recipes%20Page/reteteproprii.html">Propriile retete</a>
         </div>
       </div>
     </nav>`;
+
+document.getElementById("calendarpost").addEventListener("click", function() {
+    let dropdown = document.getElementsByClassName("dropdown-content")[0];
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+});
+
+document.getElementById("maimultelinks").addEventListener("click", function() {
+    let dropdown = document.getElementsByClassName("dropdown-content")[1];
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+});
